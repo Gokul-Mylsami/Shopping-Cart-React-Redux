34c3d83520f4a3e4bd80c01bd8ce71cc27750a5f
Gokul <@gmail.com>
2022-02-15 13:18:57 +0530

    files saved

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
+import reducer from "./reducer";
 // redux stuff
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+// reducer
+const initialStore = {
+  cart: cartItems,
+  total: 107,
+  amount: 5,
+};
 
+const store = createStore(reducer, initialStore);
+
+console.log(store.getState());
 function App() {
   // cart setup
 
   return (
-    <main>
+    <Provider store={store}>
       <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
+      <CartContainer />
+    </Provider>
   );
 }
 
